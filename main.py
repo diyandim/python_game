8b0d3896078593ff8dc517511b346d685b979e4d
diyandim <diyanmardimitrov@gmail.com>
2026-08-13 15:01:11 +0300

    added fps

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
 
